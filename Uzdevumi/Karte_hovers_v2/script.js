cf2ef0412cb4364bb991696fe3052f5de673c2c9
Gustavs <gg00186@rvt.lv>
2024-10-18 16:00:53 +0300

    16:00

--- Uzdevumi/Karte_hovers_v2/script.js
+++ Uzdevumi/Karte_hovers_v2/script.js
@@ -5,3 +5,10 @@ function togglePopup() {
         popup.classList.remove("show");
     }, 3000);
 }
+
+document.querySelector('.dropdown-content a[href="#"]').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default anchor click behavior
+    const infoBox = document.getElementById('info-box');
+    infoBox.style.display = infoBox.style.display === 'none' ? 'block' : 'none'; // Toggle visibility
+});
+
